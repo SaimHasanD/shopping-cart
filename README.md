9f0c0fb46bc36e41f750ff51675c0ac6e6308d49
SaimHasanD <saimhasan514@gmail.com>
2022-02-26 22:30:10 +0600

    minus button done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# shopping-cart
+// const inputNumber = document.getElementById("firstInput").value;
+    // const number = parseFloat(inputNumber);
+    // const totalNumber = number + 1;
+    // document.getElementById("firstInput").value = totalNumber;# shopping-cart
--- script.js
+++ script.js
@@ -4,10 +4,23 @@ document.getElementById("plusButtonOne").addEventListener("click", function(){
 document.getElementById("plusButtonTwo").addEventListener("click", function(){
     updatePlusButton("secondInput");
 })
+document.getElementById("minusButtonOne").addEventListener("click", function(){
+    updateMinusButton("firstInput");
+})
+document.getElementById("minusButtonTwo").addEventListener("click", function(){
+    updateMinusButton("secondInput");
+})
 
  function updatePlusButton(input){
     const inputNumber = document.getElementById(input).value;
     const number = parseFloat(inputNumber);
     const totalNumber = number + 1;
+    document.getElementById(input).value = totalNumber;
+ }
+
+ function updateMinusButton(input){
+    const inputNumber = document.getElementById(input).value;
+    const number = parseFloat(inputNumber);
+    const totalNumber = number - 1;
     document.getElementById(input).value = totalNumber;
  }
