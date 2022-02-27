a1cd11ef7f2a0b8f937fe16eeb1aad64e6003add
SaimHasanD <saimhasan514@gmail.com>
2022-02-27 23:14:20 +0600

    case and phone in one function

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-// phone input button
-function handleProductChanges(isIncrease){
-    const inputNumber = document.getElementById("phoneInput");
-    const number = parseFloat(inputNumber.value);
-    let totalNumber = number;
-    if (isIncrease == true) {
-        totalNumber = number + 1;
-    }
-    if (isIncrease == false && number>0) {
-        totalNumber = number - 1;
-    }
-    inputNumber.value = totalNumber;
-    const totalPrice = totalNumber * 1200;
-    document.getElementById("phonePrice").innerText = '$'+totalPrice;
-}
-// case input button
-function handleProductChange(isIncrease){
-    const inputNumber = document.getElementById("caseInput");
+function handleProductChange(product, isIncrease){
+    const inputNumber = document.getElementById(product + "Input");
     const number = parseFloat(inputNumber.value);
     let totalNumber = number;
     if (isIncrease == true) {
@@ -26,5 +10,5 @@ function handleProductChange(isIncrease){
     }
     inputNumber.value = totalNumber;
     const totalPrice = totalNumber * 59;
-    document.getElementById("casePrice").innerText = '$'+totalPrice;
+    document.getElementById(product + "Price").innerText = '$'+totalPrice;
 }
