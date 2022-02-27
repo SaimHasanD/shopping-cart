af1064854e4711a59b2a8c5cfaf52e5bd6716ab5
SaimHasanD <saimhasan514@gmail.com>
2022-02-27 22:25:21 +0600

    case minus

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ document.getElementById("casePlus").addEventListener("click", function () {
     const totalPrice = totalNumber * 59;
     document.getElementById("casePrice").innerText = '$'+totalPrice;
 })
+
+document.getElementById("caseMinus").addEventListener("click", function () {
+    const inputNumber = document.getElementById("caseInput");
+    const number = parseFloat(inputNumber.value);
+    const totalNumber = number - 1;
+    inputNumber.value = totalNumber;
+    const totalPrice = totalNumber * 59;
+    document.getElementById("casePrice").innerText = '$'+totalPrice;
+})
