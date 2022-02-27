a8edd39d6f86a4d95f59abc35a6e958ab81245aa
SaimHasanD <saimhasan514@gmail.com>
2022-02-27 22:59:19 +0600

    onclick

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-document.getElementById("phonePlus").addEventListener("click", function(){
-    handleProductChanges(true);
-})
-
-document.getElementById("phoneMinus").addEventListener("click", function(){
-    handleProductChanges(false);
-})
-
-document.getElementById("casePlus").addEventListener("click", function () {
-    handleProductChange(true);
-});
-
-document.getElementById("caseMinus").addEventListener("click", function () {
-    handleProductChange(false);
-});
-
 // phone input button
 function handleProductChanges(isIncrease){
     const inputNumber = document.getElementById("phoneInput");
     const number = parseFloat(inputNumber.value);
     let totalNumber = number;
-    // const totalNumber = number - 1;
     if (isIncrease == true) {
         totalNumber = number + 1;
     }
@@ -30,13 +13,11 @@ function handleProductChanges(isIncrease){
     const totalPrice = totalNumber * 1200;
     document.getElementById("phonePrice").innerText = '$'+totalPrice;
 }
-
 // case input button
 function handleProductChange(isIncrease){
     const inputNumber = document.getElementById("caseInput");
     const number = parseFloat(inputNumber.value);
     let totalNumber = number;
-    // const totalNumber = number - 1;
     if (isIncrease == true) {
         totalNumber = number + 1;
     }
