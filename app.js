5438c739c9ef7ea19965b9bd76ef87951e3eabeb
SaimHasanD <saimhasan514@gmail.com>
2022-02-27 22:32:44 +0600

    case plus, case minus

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 document.getElementById("casePlus").addEventListener("click", function () {
+    handleProductChanges();
+});
+
+document.getElementById("caseMinus").addEventListener("click", function () {
+    handleProductChange();
+});
+
+function handleProductChanges(){
     const inputNumber = document.getElementById("caseInput");
     const number = parseFloat(inputNumber.value);
     const totalNumber = number + 1;
     inputNumber.value = totalNumber;
     const totalPrice = totalNumber * 59;
     document.getElementById("casePrice").innerText = '$'+totalPrice;
-})
+}
 
-document.getElementById("caseMinus").addEventListener("click", function () {
+function handleProductChange(){
     const inputNumber = document.getElementById("caseInput");
     const number = parseFloat(inputNumber.value);
     const totalNumber = number - 1;
     inputNumber.value = totalNumber;
     const totalPrice = totalNumber * 59;
     document.getElementById("casePrice").innerText = '$'+totalPrice;
-})
+}
+
+// document.getElementById("casePlus").addEventListener("click", function () {
+//     const inputNumber = document.getElementById("caseInput");
+//     const number = parseFloat(inputNumber.value);
+//     const totalNumber = number + 1;
+//     inputNumber.value = totalNumber;
+//     const totalPrice = totalNumber * 59;
+//     document.getElementById("casePrice").innerText = '$'+totalPrice;
+// });
+
+// document.getElementById("caseMinus").addEventListener("click", function () {
+//     const inputNumber = document.getElementById("caseInput");
+//     const number = parseFloat(inputNumber.value);
+//     const totalNumber = number - 1;
+//     inputNumber.value = totalNumber;
+//     const totalPrice = totalNumber * 59;
+//     document.getElementById("casePrice").innerText = '$'+totalPrice;
+// });
