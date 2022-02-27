90a4cc396148c546d3f4cec323736a3658544ff2
SaimHasanD <saimhasan514@gmail.com>
2022-02-27 22:23:19 +0600

    new script

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 // const inputNumber = document.getElementById("firstInput").value;
     // const number = parseFloat(inputNumber);
     // const totalNumber = number + 1;
-    // document.getElementById("firstInput").value = totalNumber;# shopping-cart
+    // document.getElementById("firstInput").value = totalNumber;
+    # shopping-cart
+    if (isIncrease == true) {
+        totalNUmber = number + 1;
+    }
+    if (isIncrease == false) {
+        totalNUmber = number - 1; 
+    }
--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 document.getElementById("plusButtonOne").addEventListener("click", function(){
-    updatePlusButton("firstInput");
+    // updatePlusButton("firstInput");
  })
 document.getElementById("plusButtonTwo").addEventListener("click", function(){
-    updatePlusButton("secondInput");
+    // updatePlusButton("secondInput");
 })
 document.getElementById("minusButtonOne").addEventListener("click", function(){
-    updateMinusButton("firstInput");
+    // updateMinusButton("firstInput");
 })
 document.getElementById("minusButtonTwo").addEventListener("click", function(){
-    updateMinusButton("secondInput");
+    const inputNumber = document.getElementById("secondInput");
+    const number = parseFloat(inputNumber.value);
+    const totalNumber = number - 1;
+    inputNumber.value = totalNumber;
+    const totalPrice = totalNumber * 59;
+    document.getElementById('casePrice').innerHTML = totalPrice ;
+    // updateMinusButton("secondInput");
 })
 
- function updatePlusButton(input){
-    const inputNumber = document.getElementById(input).value;
-    const number = parseFloat(inputNumber);
+ function updatePlusButton(){
+    const inputNumber = document.getElementById("secondInput");
+    const number = parseFloat(inputNumber.value);
     const totalNumber = number + 1;
-    document.getElementById(input).value = totalNumber;
+    inputNumber.value = totalNumber;
+    const totalPrice = totalNumber * 59;
+    document.getElementById('casePrice').innerText = totalPrice ;
  }
 
- function updateMinusButton(input){
-    const inputNumber = document.getElementById(input).value;
-    const number = parseFloat(inputNumber);
-    const totalNumber = number - 1;
-    document.getElementById(input).value = totalNumber;
+ function updateMinusButton(){
+    // const inputNumber = document.getElementById("secondInput");
+    // const number = parseFloat(inputNumber.value);
+    // const totalNumber = number - 1;
+    // inputNumber.value = totalNumber;
+    // const totalPrice = totalNumber * 59;
+    // document.getElementById('casePrice').innerHTML = totalPrice ;
+
  }
+ 
+    // const inputNumber = document.getElementById("firstInput").value;
+    // const number = parseFloat(inputNumber);
+    // const totalNumber = number + 1;
+    // document.getElementById("firstInput").value = totalNumber;
